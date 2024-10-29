780342f4b3d25b4e0beff01f0a9c8552379d5a31
Cotbert <mateojgdev@gmail.com>
2024-10-29 12:02:38 -0500

    utils functions

--- src/backend/utils/utils.py
+++ src/backend/utils/utils.py
@@ -13,10 +13,17 @@ def validatePathPdf(url):
         return True
     return False
 
+def complete_pdf_name_file(filename) -> str:
+    return filename if filename.endswith('.pdf') else filename + '.pdf'
 
-def complete_file_path(file)  -> str:
-    file_len = len(file)
-    if (file[file_len - 4: file_len] != ".pdf"):
-        file += ".pdf"
-    return file
+
+def validate_pdf_pages(filename, page_slicer) -> bool:
+    if page_slicer <= 0:
+        return False
+    file = pymupdf.open(filename=filename)
+    if page_slicer >= file.page_count:
+        return False
+    return True
+
+
 
