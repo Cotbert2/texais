c2588a3d24daf8c4ff9e481e40cc9646a7179321
Cotbert2 <mateojgdev@gmail.com>
2024-12-20 08:43:25 -0500

    nav bar

---
+++ src/frontend/texais-frontend/src/app/components/views/lock/lock.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lock',
+  imports: [],
+  templateUrl: './lock.component.html',
+  styleUrl: './lock.component.scss'
+})
+export class LockComponent {
+
+}
