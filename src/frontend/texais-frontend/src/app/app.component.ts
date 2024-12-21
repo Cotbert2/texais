5714192d8ad2d2a608fadb4668c1f9b082c641ed
Cotbert2 <mateojgdev@gmail.com>
2024-12-21 15:52:40 -0500

    system information in merge

--- src/frontend/texais-frontend/src/app/app.component.ts
+++ src/frontend/texais-frontend/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from "./main/main.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,35 @@ import { FooterComponent } from "./components/footer/footer.component";
     RouterOutlet,
     NavbarComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    ToastModule,
+
 ],
+providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    console.log('app component');
+
+  }
+
+  constructor(
+    private messageService: MessageService
+  ){}
+
+
   title = 'texais-frontend';
   activeItem : string = 'home';
 
+
   changeActiveTabService(service : string ) : void {
     this.activeItem = service;
+  }
+
+  newMessage(kind : string, sumary : string, datail : string) : void {
+    this.messageService.add({severity: kind, summary: sumary, detail: datail});
   }
 }
--- src/frontend/texais-frontend/src/app/components/views/merge/merge.component.ts
+++ src/frontend/texais-frontend/src/app/components/views/merge/merge.component.ts
@@ -1,23 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CSP_NONCE, OnInit } from '@angular/core';
 import { FrameLogoComponent } from "../../frame-logo/frame-logo.component";
 import { FileUploadModule } from 'primeng/fileupload';
 import { CommonModule } from '@angular/common';
 import { PdfServiceService } from '../../../services/pdf-service.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Button, ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { AppComponent } from '../../../app.component';
+import { ProgressBar } from 'primeng/progressbar';
+
+import { ToastServiceService } from '../../../services/toast-service.service';
 
 @Component({
   selector: 'app-merge',
-  imports: [FrameLogoComponent, FileUploadModule, CommonModule, HttpClientModule],
+  imports: [FrameLogoComponent,
+    FileUploadModule,
+    CommonModule,
+    HttpClientModule,
+    ButtonModule,
+    TableModule,
+    ProgressBar
+  ],
   templateUrl: './merge.component.html',
   styleUrl: './merge.component.scss'
 })
 export class MergeComponent implements OnInit{
-  constructor(private pdfService: PdfServiceService) { }
+  constructor(
+    private pdfService: PdfServiceService,
+    private toastService: ToastServiceService,
+    private appComponent: AppComponent
+  ) { }
 
   uploadedFiles: any[] = [];
   currentFiles : any[] = [];
+  cols : any[] = [];
+  products : any[] = [];
+  isMerging : boolean = false;
+  progress : number = 0;
+  isFileAvailableToDownload : boolean = false;
 
   ngOnInit(): void {
+
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      { field: 'size', header: 'Size' },
+      { field: 'type', header: 'Type' }
+    ];
+    this.startProgress();
+
+  }
+
+  startProgress() : void {
+    setInterval(() => {
+      if (this.progress < 70) {
+        this.progress += 1;
+      }
+    }, 100);
   }
 
   onUpload(event : any) : void {
@@ -28,6 +66,7 @@ export class MergeComponent implements OnInit{
     console.log('upload files', event);
     this.currentFiles = event.currentFiles;
     console.log('current files', this.currentFiles);
+    this.uploadProducts();
 
   }
 
@@ -35,6 +74,27 @@ export class MergeComponent implements OnInit{
     console.log('upload delete files', event);
     this.currentFiles = this.currentFiles.filter(file => file.name !== event.file.name);
     console.log('current files', this.currentFiles);
+    this.uploadProducts();
+  }
+
+
+  mergeFiles() : void {
+    console.log('merge files');
+    console.log('current files', this.products);
+    console.log('sould merge files');
+    if (this.currentFiles.length < 2) {
+      this.appComponent.newMessage('warn', 'Cannot Merge', 'You need to select at least 2 files to merge');
+    }
+  }
+
+  uploadProducts() : void {
+    this.products = this.currentFiles.map((currentFile : any ) => {
+      return {
+        name: currentFile.name,
+        size: currentFile.size,
+        type: currentFile.type
+      };
+    });
   }
 
 
